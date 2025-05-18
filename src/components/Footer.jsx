469a753a6ff398b0a9c9875fe3839e71ebf42b5d
Aryan Sharma <76253876+Aryan110303@users.noreply.github.com>
2025-05-19 01:50:02 +0530

    Footer Fix

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ import logo from '../assets/logo.jpg'
 
 function Footer() {
   return (
-    <footer className=' border-t border-border mt-22'>
+    <footer className='border-t border-border mt-24'>
         <div className='container flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0 mx-auto px-6'>
             <div className='flex flex-col items-center gap-4 md:flex-row md:px-0 px-8 md:gap-2'>
                 <img src={logo} alt="" width={34} height={36}/>
